f10e251024379768db4bcdc9f872312dab744cf2
lexanow <lexanow@gmail.com>
2022-11-20 19:11:21 +0300

    Cycles

--- src/TableMulti.java
+++ src/TableMulti.java
@@ -3,8 +3,13 @@
  */
 public class TableMulti {
     public static void main(String[] args) {
-        for (int i = 1; i < 10; i++) {
 
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                int a = i * j;
+                System.out.printf("%d  ", a);
+            }
+            System.out.println();
         }
     }
 }
